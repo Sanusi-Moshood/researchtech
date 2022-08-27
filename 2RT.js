aecc8f3a99310592f80634b7eb2bbfa7927c3e80
Moshood Sanusi <snsmoshood@gmail.com>
2022-08-27 10:14:53 +0100

    First client review

---
+++ 2RT.js
@@ -0,0 +1,18 @@
+var burger = document.querySelector('.burger');
+var nav = document.querySelector(".nav-links");
+var icon = document.querySelector('.icon').src
+
+var Toggle = false
+
+burger.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('click')
+    nav.classList.toggle('nav-active');
+   if (Toggle === false){
+    document.querySelector('.icon').src = "Frame 38.svg"
+   }else {
+    document.querySelector('.icon').src = "Frame 37.svg"
+   }
+    
+   Toggle = !Toggle
+})
